15aa94b85d7d7494ba8b131b1329af49338c19ab
Erick Nevarez <ericknevarez97@gmail.com>
2025-02-21 21:31:27 -0600

    code i forgot

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ def generate_board():
     print(scout_entry.get())
     print(combine_entry.get())
 
-    custom_values = retrieve.retrieve_custom_values()
-
+    
     players = process.map_players(scout_entry.get(), custom_values)
     combine_min_max_map= process.map_combine(players, combine_entry.get())
 
@@ -51,7 +50,12 @@ def generate_board():
         # Reconvert the broad_jump because im not mean
         # Probably move this
         player.combine[0][2] = Exercise("broad_jump", process.convert_float_to_feet(player.combine[0][2].value))
-    
+        
+        # Do the final total score for csv sorting, with user weights
+        potential_weighted = player.potential_weighted * custom_values.overall_weight
+        ras_score = player.ras_score * custom_values.ras_weight
+        report_score = player.report_score * custom_values.report_weight
+        player.total_score = potential_weighted + ras_score + report_score + player.culture_score
 
     sorted_players = sorted(players.values(), key=lambda player: player.total_score, reverse=True)
     report.generate_board(sorted_players)
